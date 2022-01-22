48d19ccb62d04d0e179c527647f00bd10c15bca8
dpisati <43629737+dpisati@users.noreply.github.com>
2022-01-22 20:53:37 +1300

    skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, FormEvent, KeyboardEvent, useState } from "react";
+
+import styles from "./Styles.module.css";
+
+interface FinalWord {
+    letter: string;
+    contais: boolean;
+    rightPlace: boolean;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [word, setWord] = useState<string>("");
+    const [tries, setTries] = useState<FinalWord[][]>([]);
+    const goalWord = ["i", "d", "e", "i", "a"];
+
+    function wordBrakdown() {
+        const letters = word.split("");
+        const newArray: FinalWord[] = [];
+        letters.forEach((letter, index) => {
+            const isInTheRightPlace = letter === goalWord[index];
+            const obj: FinalWord = {
+                letter,
+                contais: goalWord.includes(letter),
+                rightPlace: isInTheRightPlace,
+            };
+            newArray.push(obj);
+        });
+        setTries([...tries, newArray]);
+    }
+
+    function handleKeyDown(e: KeyboardEvent<HTMLInputElement>) {
+        if (e.key === "Enter" && word.length === 5) {
+            wordBrakdown();
+            setWord("");
+        }
+    }
+
+    function isOver() {
+        let rightLetter = [];
+        if (tries.length > 0) {
+            tries[tries.length - 1].forEach((letter) => {
+                if (letter.rightPlace) {
+                    rightLetter.push(letter.letter);
+                }
+            });
+
+            if (rightLetter.length === 5) {
+                return true;
+            }
+
+            if (tries.length === 6) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function handleChange(e: ChangeEvent<HTMLInputElement>) {
+        setWord(e.target.value.replace(/[^a-zA-Z]/gi, ""));
+    }
+
+    return (
+        <>
+            <h1>Woordle</h1>
+
+            {!isOver() && (
+                <input
+                    value={word}
+                    className={styles.input}
+                    onChange={(e) => handleChange(e)}
+                    onKeyDown={(e) => handleKeyDown(e)}
+                    type="text"
+                    maxLength={5}
+                />
+            )}
+
+            <div className={styles.triesMainContainer}>
+                {tries.map((guess) => (
+                    <div key={Math.random()} className={styles.triesContainer}>
+                        {guess.map((letter) => (
+                            <p
+                                key={letter.letter + Math.random()}
+                                className={`${styles.letter} ${
+                                    letter.contais && styles.contains
+                                } ${letter.rightPlace && styles.rightPlace}`}
+                            >
+                                {letter.letter}
+                            </p>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }
 
 export default App;
